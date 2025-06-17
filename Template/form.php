550406be2cafcb4e961e3852a8974a2dc37b764c
prithvi13-png <prithviprabhakarh13@gmail.com>
2025-06-17 22:05:13 +0530

    ui setup

--- Template/form.php
+++ Template/form.php
@@ -8,7 +8,7 @@
 	$message = isset( $_POST['feedbackMessage'] ) ? preg_replace( "/(From:|To:|BCC:|CC:|Subject:|Content-Type:)/", "", $_POST['feedbackMessage'] ) : "";
 
 	//Headers
-	$to = "youremail@domain.com";
+	$to = "vedhouseconstructions@gmail.com";
     $subject = 'Contact Us';
 	$headers = "MIME-Version: 1.0\r\n";
 	$headers .= "Content-type: text/html; charset=iso-8859-1\r\n";
